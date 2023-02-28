29a4ca8d94f4e959148e459d3e4f7c6c0f14eb7b
Alex Toofanian <alextoofanian@gmail.com>
2023-02-28 05:37:24 +0000

    linear satellite

--- odp/dynamics/linear_satellite.py
+++ odp/dynamics/linear_satellite.py
@@ -21,12 +21,30 @@ class LinearSatelliteD(OdpDynamics):
         x5_dot = hcl.scalar(0, 'x5_dot')
         x6_dot = hcl.scalar(0, 'x6_dot')
 
+        # f(x)
         x1_dot[0] = state[4]
         x2_dot[0] = state[5]
         x3_dot[0] = state[6]
-        x4_dot[0] = 3 * hcl.sqrt(self.mu / (a * a * a)) * hcl.sqrt(self.mu / (a * a * a)) * state[0] + 2 * hcl.sqrt(self.mu / (a * a * a)) * state[4]
-        x5_dot[0] = -2 * hcl.sqrt(self.mu / (a * a * a)) * state[3]
-        x6_dot[0] = -(hcl.sqrt(self.mu / (a * a * a)) * hcl.sqrt(self.mu / (a * a * a))) * state[2]
+        x4_dot[0] = 3 * hcl.sqrt(self.mu / (self.a * self.a * self.a)) * hcl.sqrt(self.mu / (self.a * self.a * self.a)) * state[0] + 2 * hcl.sqrt(self.mu / (self.a * self.a * self.a)) * state[4] + self.ux_target
+        x5_dot[0] = -2 * hcl.sqrt(self.mu / (self.a * self.a * self.a)) * state[3] + self.uy_target
+        x6_dot[0] = -(hcl.sqrt(self.mu / (self.a * self.a * self.a)) * hcl.sqrt(self.mu / (self.a * self.a * self.a))) * state[2] + self.uz_target
+
+        # ... + G_u(x) @ u
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+        x1_dot[0] = x1_dot[0] + u_opt[0]
+        x1_dot[0] = x1_dot[0] + u_opt[1]
+        x1_dot[0] = x1_dot[0] + u_opt[2]
+
+        # ... + G_d(x) @ d
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+        # x1_dot[0] = x1_dot[0]
+
         return x1_dot[0], x2_dot[0], x3_dot[0], x4_dot[0], x5_dot[0], x6_dot[0]
 
     def opt_ctrl(self, t, state, spat_deriv):
@@ -48,60 +66,60 @@ class LinearSatelliteD(OdpDynamics):
 
         MaxDV = hcl.scalar(0, "MaxDV")
 
-        SumUUU = ...
-        SumUUL = ...
-        SumULU = ...
-        SumLUU = ...
-        SumULL = ...
-        SumLUL = ...
-        SumLLU = ...
-        SumLLL = ...
+        SumUUU = spat_deriv[3] * self.max_control[0] + spat_deriv[4] * self.max_control[1] + spat_deriv[5] * self.max_control[2]
+        SumUUL = spat_deriv[3] * self.max_control[0] + spat_deriv[4] * self.max_control[1] + spat_deriv[5] * self.min_control[2]
+        SumULU = spat_deriv[3] * self.max_control[0] + spat_deriv[4] * self.min_control[1] + spat_deriv[5] * self.max_control[2]
+        SumLUU = spat_deriv[3] * self.min_control[0] + spat_deriv[4] * self.max_control[1] + spat_deriv[5] * self.max_control[2]
+        SumULL = spat_deriv[3] * self.max_control[0] + spat_deriv[4] * self.min_control[1] + spat_deriv[5] * self.min_control[2]
+        SumLUL = spat_deriv[3] * self.min_control[0] + spat_deriv[4] * self.max_control[1] + spat_deriv[5] * self.min_control[2]
+        SumLLU = spat_deriv[3] * self.min_control[0] + spat_deriv[4] * self.min_control[1] + spat_deriv[5] * self.max_control[2]
+        SumLLL = spat_deriv[3] * self.min_control[0] + spat_deriv[4] * self.min_control[1] + spat_deriv[5] * self.min_control[2]
 
         MaxDV[0] = SumUUU[0]
-        uOpt1[0] = self.max_thrust[0]
-        uOpt2[0] = self.max_thrust[1]
-        uOpt3[0] = self.max_thrust[2]
+        uOpt1[0] = self.max_control[0]
+        uOpt2[0] = self.max_control[1]
+        uOpt3[0] = self.max_control[2]
 
         with hcl.if_(SumUUL[0] > MaxDV[0]):
-            uOpt1[0] = self.max_thrust[0]
-            uOpt2[0] = self.max_thrust[1]
-            uOpt2[0] = self.min_thrust[2]
+            uOpt1[0] = self.max_control[0]
+            uOpt2[0] = self.max_control[1]
+            uOpt2[0] = self.min_control[2]
             MaxDV[0] = SumUUL[0]
 
         with hcl.elif_(SumULU[0] > MaxDV[0]):
-            uOpt1[0] = self.max_thrust[0]
-            uOpt2[0] = self.min_thrust[1]
-            uOpt2[0] = self.max_thrust[2]
+            uOpt1[0] = self.max_control[0]
+            uOpt2[0] = self.min_control[1]
+            uOpt2[0] = self.max_control[2]
             MaxDV[0] = SumULU[0]
 
         with hcl.elif_(SumLUU[0] > MaxDV[0]):
-            uOpt1[0] = self.min_thrust[0]
-            uOpt2[0] = self.max_thrust[1]
-            uOpt2[0] = self.max_thrust[2]
+            uOpt1[0] = self.min_control[0]
+            uOpt2[0] = self.max_control[1]
+            uOpt2[0] = self.max_control[2]
             MaxDV[0] = SumLUU[0]
 
         with hcl.elif_(SumULL[0] > MaxDV[0]):
-            uOpt1[0] = self.max_thrust[0]
-            uOpt2[0] = self.min_thrust[1]
-            uOpt2[0] = self.min_thrust[2]
+            uOpt1[0] = self.max_control[0]
+            uOpt2[0] = self.min_control[1]
+            uOpt2[0] = self.min_control[2]
             MaxDV[0] = SumULL[0]
 
         with hcl.elif_(SumLUL[0] > MaxDV[0]):
-            uOpt1[0] = self.min_thrust[0]
-            uOpt2[0] = self.max_thrust[1]
-            uOpt2[0] = self.min_thrust[2]
+            uOpt1[0] = self.min_control[0]
+            uOpt2[0] = self.max_control[1]
+            uOpt2[0] = self.min_control[2]
             MaxDV[0] = SumLUL[0]
 
         with hcl.elif_(SumLLU[0] > MaxDV[0]):
-            uOpt1[0] = self.min_thrust[0]
-            uOpt2[0] = self.min_thrust[1]
-            uOpt2[0] = self.max_thrust[2]
+            uOpt1[0] = self.min_control[0]
+            uOpt2[0] = self.min_control[1]
+            uOpt2[0] = self.max_control[2]
             MaxDV[0] = SumLLU[0]
 
         with hcl.elif_(SumLLL[0] > MaxDV[0]):
-            uOpt1[0] = self.min_thrust[0]
-            uOpt2[0] = self.min_thrust[1]
-            uOpt2[0] = self.min_thrust[2]
+            uOpt1[0] = self.min_control[0]
+            uOpt2[0] = self.min_control[1]
+            uOpt2[0] = self.min_control[2]
             MaxDV[0] = SumLLL[0]
 
         return uOpt1[0], uOpt2[0], uOpt3[0], uOpt4[0], uOpt5[0], uOpt6[0]
