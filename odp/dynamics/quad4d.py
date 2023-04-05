085223c080a8cc60486957ed7aba5ae9e29f78e0
Alex Toofanian <alextoofanian@gmail.com>
2023-04-05 04:52:20 +0000

    Added periodic dimensions to quad4d

--- odp/dynamics/quad4d.py
+++ odp/dynamics/quad4d.py
@@ -1,3 +1,4 @@
+from typing import List
 import heterocl as hcl
 
 from odp.dynamics.abstract_dynamics import OdpDynamics
@@ -7,24 +8,31 @@ class Quad4D(OdpDynamics):
     def __init__(self):
         self.gravity: float = 9.81
         self.mass: float = 2.5
-        self.drag_coefficient_v: float = .25
-        self.drag_coefficient_phi: float = .02255
+        self.drag_coefficient_v: float = 0.25
+        self.drag_coefficient_phi: float = 0.02255
         self.length_between_copters: float = 1.0
         self.moment_of_inertia: float = 1.0
+
+        self.periodic_state_dimensions: List[int] = [2]
 
         self.min_thrust: float = 0
         self.max_thrust: float = 18.39375
 
     def dynamics(self, t, state, u_opt, d_opt):
-        x1_dot = hcl.scalar(0, 'x1_dot')
-        x2_dot = hcl.scalar(0, 'x2_dot')
-        x3_dot = hcl.scalar(0, 'x3_dot')
-        x4_dot = hcl.scalar(0, 'x4_dot')
+        x1_dot = hcl.scalar(0, "x1_dot")
+        x2_dot = hcl.scalar(0, "x2_dot")
+        x3_dot = hcl.scalar(0, "x3_dot")
+        x4_dot = hcl.scalar(0, "x4_dot")
 
-        x1_dot[0] = (state[1])
-        x2_dot[0] = (-self.drag_coefficient_v/self.mass * state[1] - self.gravity) + ((hcl.cos(state[2]) / self.mass * u_opt[0]) + (hcl.cos(state[2]) / self.mass * u_opt[1]))
-        x3_dot[0] = (state[3])
-        x4_dot[0] = (-self.drag_coefficient_phi / self.moment_of_inertia * state[3]) + ((-self.length_between_copters / self.moment_of_inertia * u_opt[0]) + (self.length_between_copters / self.moment_of_inertia * u_opt[1]))
+        x1_dot[0] = state[1]
+        x2_dot[0] = (-self.drag_coefficient_v / self.mass * state[1] - self.gravity) + (
+            (hcl.cos(state[2]) / self.mass * u_opt[0]) + (hcl.cos(state[2]) / self.mass * u_opt[1])
+        )
+        x3_dot[0] = state[3]
+        x4_dot[0] = (-self.drag_coefficient_phi / self.moment_of_inertia * state[3]) + (
+            (-self.length_between_copters / self.moment_of_inertia * u_opt[0])
+            + (self.length_between_copters / self.moment_of_inertia * u_opt[1])
+        )
         return x1_dot[0], x2_dot[0], x3_dot[0], x4_dot[0]
 
     def opt_ctrl(self, t, state, spat_deriv):
@@ -40,10 +48,38 @@ class Quad4D(OdpDynamics):
 
         MaxDV = hcl.scalar(0, "MaxDV")
 
-        SumUU[0] = spat_deriv[1]*(((hcl.cos(state[2]) / self.mass * self.max_thrust) + (hcl.cos(state[2]) / self.mass * self.max_thrust))) + spat_deriv[3]*(((-self.length_between_copters / self.moment_of_inertia * self.max_thrust) + (self.length_between_copters / self.moment_of_inertia * self.max_thrust)))
-        SumUL[0] = spat_deriv[1]*(((hcl.cos(state[2]) / self.mass * self.max_thrust) + (hcl.cos(state[2]) / self.mass * self.min_thrust))) + spat_deriv[3]*(((-self.length_between_copters / self.moment_of_inertia * self.max_thrust) + (self.length_between_copters / self.moment_of_inertia * self.min_thrust)))
-        SumLU[0] = spat_deriv[1]*(((hcl.cos(state[2]) / self.mass * self.min_thrust) + (hcl.cos(state[2]) / self.mass * self.max_thrust))) + spat_deriv[3]*(((-self.length_between_copters / self.moment_of_inertia * self.min_thrust) + (self.length_between_copters / self.moment_of_inertia * self.max_thrust)))
-        SumLL[0] = spat_deriv[1]*(((hcl.cos(state[2]) / self.mass * self.min_thrust) + (hcl.cos(state[2]) / self.mass * self.min_thrust))) + spat_deriv[3]*(((-self.length_between_copters / self.moment_of_inertia * self.min_thrust) + (self.length_between_copters / self.moment_of_inertia * self.min_thrust)))
+        SumUU[0] = spat_deriv[1] * (
+            ((hcl.cos(state[2]) / self.mass * self.max_thrust) + (hcl.cos(state[2]) / self.mass * self.max_thrust))
+        ) + spat_deriv[3] * (
+            (
+                (-self.length_between_copters / self.moment_of_inertia * self.max_thrust)
+                + (self.length_between_copters / self.moment_of_inertia * self.max_thrust)
+            )
+        )
+        SumUL[0] = spat_deriv[1] * (
+            ((hcl.cos(state[2]) / self.mass * self.max_thrust) + (hcl.cos(state[2]) / self.mass * self.min_thrust))
+        ) + spat_deriv[3] * (
+            (
+                (-self.length_between_copters / self.moment_of_inertia * self.max_thrust)
+                + (self.length_between_copters / self.moment_of_inertia * self.min_thrust)
+            )
+        )
+        SumLU[0] = spat_deriv[1] * (
+            ((hcl.cos(state[2]) / self.mass * self.min_thrust) + (hcl.cos(state[2]) / self.mass * self.max_thrust))
+        ) + spat_deriv[3] * (
+            (
+                (-self.length_between_copters / self.moment_of_inertia * self.min_thrust)
+                + (self.length_between_copters / self.moment_of_inertia * self.max_thrust)
+            )
+        )
+        SumLL[0] = spat_deriv[1] * (
+            ((hcl.cos(state[2]) / self.mass * self.min_thrust) + (hcl.cos(state[2]) / self.mass * self.min_thrust))
+        ) + spat_deriv[3] * (
+            (
+                (-self.length_between_copters / self.moment_of_inertia * self.min_thrust)
+                + (self.length_between_copters / self.moment_of_inertia * self.min_thrust)
+            )
+        )
 
         MaxDV[0] = SumUU[0]
         uOpt1[0] = self.max_thrust
@@ -72,4 +108,3 @@ class Quad4D(OdpDynamics):
         d4 = hcl.scalar(0, "d4")
 
         return d1[0], d2[0], d3[0], d4[0]
-
